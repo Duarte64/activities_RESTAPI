848368a388f1823dd2740d5ac7456158e522db39
Gabriel Duarte <gabriel.duartepaz@gmail.com>
2022-09-14 03:19:33 -0300

    bugfix: Associa rotas aos controladores e ajusta delete de User.

--- routes/activities.js
+++ routes/activities.js
@@ -1,72 +1,33 @@
 const express = require('express');
-const Activity = require('../models/activity');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const activities = await Activity.find().populate("user");
-        if (activities) {
-            res.status(200).json(activities);
-        } else {
-            res.status(204).json([]);
-        }
-    } catch (err) {
-        res.json({ message: err });
-    }
+const { 
+    getActivities,
+    createActivity,
+    updateActivity,
+    deleteActivity
+} = require('../controllers/activityController');
+
+router.get('/', (req, res) => {
+    getActivities(req, res);
 });
 
 router.get('/:_id', async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const activity = await Activity.findById(_id).populate("user");
-        if (activity) {
-            res.status(200).json(activity);
-        } else {
-            res.status(204).json({});
-        }
-    } catch (err) {
-        res.json({ message: err });
-    }
+    getActivities(req, res);
 });
 
-router.post('/', async (req, res) => {
-    const { user, title, duration, date } = req.body;
-    const activity = new Activity({
-        user,
-        title,
-        duration,
-        date
-    });
-    try {
-        const savedActivity = await activity.save();
-        res.status(201).json(savedActivity);
-    } catch (err) {
-        res.json({ message: err });
-    }
+router.post('/', (req, res) => {
+    createActivity(req, res);
 });
 
-router.patch('/:_id', async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const updatedActivity = await Activity.updateOne(
-            {_id}, 
-            {$set: { ...req.body } }
-        );
-        res.status(200).json(updatedActivity);
-    } catch (err) {
-        res.json({message: err.message})
-    }
+router.patch('/:_id', (req, res) => {
+    updateActivity(req, res);
+    
 });
 
 router.delete('/:_id', async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const deletedActivity = await Activity.deleteOne({ _id });
-        res.status(200).json(deletedActivity);
-    } catch (err) {
-        res.json({ message: err });
-    }
+    deleteActivity(req, res);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,72 +1,32 @@
 const express = require('express');
-const User = require('../models/User');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const patients = await User.find();
-        if (patients) {
-            res.status(200).json(patients);
-        } else {
-            res.status(204).json([]);
-        }
-    } catch (err) {
-        res.json({ message: err });
-    }
+const { 
+    getUsers, 
+    createUser, 
+    updateUser, 
+    deleteUser 
+} = require('../controllers/userController');
+
+router.get('/', (req, res) => {
+    getUsers(req, res);
 });
 
 router.get('/:_id', async (req, res) => {
-    const { _id } = req.params;  
-    try {
-        const patient = await User.findById(_id);
-        if (patient) {
-            res.status(200).json(patient);
-        } else {
-            res.status(204).json({});
-        }
-    } catch (err) {
-        res.json({ message: err });
-    }
+    getUsers(req, res);
 });
 
 router.post('/', async (req, res) => {
-    const { name, birthdate, email, observations } = req.body;
-    const user = new User({
-        name,
-        birthdate,
-        email,
-        observations
-    });
-    try {
-        const savedUser = await user.save();
-        res.status(201).json(savedUser);
-    } catch (err) {
-        res.json({ message: err });
-    }
+    createUser(req, res);
 });
 
 router.patch('/:_id', async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const updatedPatient = await User.updateOne(
-            { _id },
-            { $set: {...req.body} }
-        );
-        res.status(200).json(updatedPatient);
-    } catch (err) {
-        res.json({ message: err });
-    }
+    updateUser(req, res);	
 });
 
 router.delete('/:_id', async (req, res) => {
-    const { _id } = req.params; 
-    try {
-        const removedPatient = await User.deleteOne({ _id });
-        res.status(200).json(removedPatient);
-    } catch (err) {
-        res.json({ message: err });
-    }
+    deleteUser(req, res);
 });
 
 module.exports = router;
