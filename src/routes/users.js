327121d8a539eed9a5fe1fb293822064c54299c7
Gabriel Duarte <gabriel.duartepaz@gmail.com>
2022-12-11 22:24:30 -0300

    bugfix: Melhora legibilidade da página de users.

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,25 +4,13 @@ const router = express.Router();
 
 const UserController = require('../app/controllers/userController');
 
-// router.route('/')
-//     .all() -> Auth entraria aqui por exemplo!
-//     .get(UserController.index)
-//     .post(UserController.store);
+router.route('/')
+    .get(UserController.index)
+    .post(UserController.store);
 
-// router.route('/:_id')
-//     .all() -> Auth entraria aqui por exemplo!
-//     .get(UserController.show)
-//     .patch(UserController.update)
-//     .delete(UserController.delete);
-
-router.get('/', UserController.index);
-
-router.get('/:_id', UserController.show);
-
-router.post('/', UserController.store);
-
-router.patch('/:_id', UserController.update);
-
-router.delete('/:_id', UserController.delete);
+router.route('/:_id')
+    .get(UserController.show)
+    .patch(UserController.update)
+    .delete(UserController.delete);
 
 module.exports = router;
